a0376aaa09f583d3b1ea4ec8b4204c73c499d7c2
root <root@LAPTOP-BN8HL2TP.>
2025-02-23 08:32:55 +0000

    SQL Script Editing

---
+++ Backend/MySQL/user_schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE chat_app;
+
+USE chat_app;
+
+CREATE TABLE users (
+    id 			BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username 	VARCHAR(50) NOT NULL UNIQUE,
+    email       VARCHAR(132) NOT NULL UNIQUE,
+    password 	VARCHAR(255) NOT NULL,
+    bio         TEXT DEFAULT '',
+    created_at	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_seen   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE messages (
+    id 			BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    sender_id 	BIGINT UNSIGNED NOT NULL,
+    reciever_id BIGINT UNSIGNED NOT NULL,
+    message 	TEXT NOT NULL,
+    file_url    VARCHAR(225) DEFAULT NULL,
+    status      ENUM('sent', 'delivered', 'read') DEFAULT 'sent',
+    sent_at 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	
+    FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (reciever_id) REFERENCES users(id) ON DELETE CASCADE
+); 
+
