ef1aed44f54dd92d7ab96b0210a3fef3bbb382fe
arnav0405 <arnavwaghdhare@gmail.com>
2025-03-12 20:42:12 +0000

    Tried to connect front with back

--- Backend/api/register.php
+++ Backend/api/register.php
@@ -1,5 +1,10 @@
 <?php
 require_once("../includes/db.php");
+header("Access-Control-Allow-Origin: http://localhost:5500");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
