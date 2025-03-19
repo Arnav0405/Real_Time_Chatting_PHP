f6e0fcbd6177c8eccbc1d421e181f1faf4233845
arnav0405 <arnavwaghdhare@gmail.com>
2025-03-19 19:26:36 +0000

    Log In Works!!!!!!!!!

--- Backend/includes/db.php
+++ Backend/includes/db.php
@@ -7,7 +7,7 @@ $password = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Database connected!\n";
+    // echo "Database connected!\n";
 } catch (PDOException $e) {
     die("Database connection failed:". $e->getMessage());
 }
